500f320b95884d28f8e5983dd83b112012604efb
Noa-Mari <Noa.mari123@gmail.com>
2025-12-13 00:08:30 +0200

    chore/env ready to use

--- to-do-list/App.js
+++ to-do-list/App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {View, Text} from "react-native";
+import {useNavigation, NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {Button} from "@react-navigation/elements";
 
-export default function App() {
+function HomeScreen() {
+  const navigation = useNavigation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate("Details")}>
+        Go to Details
+      </Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Text>Details Screen</Text>
+      <Button onPress={() => navigation.navigate("Home")}>Go to Home</Button>
+      <Button onPress={() => navigation.popToTop()}>Go to whatever</Button>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {backgroundColor: "tomato"},
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{title: "Overview"}}
+      />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}
